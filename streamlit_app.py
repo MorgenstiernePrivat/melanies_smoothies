3df981332097363f525e04b2ed6361162e905852
MorgenstiernePrivat <marcus.morgenstierne@icloud.com>
2026-07-31 13:56:57 +0200

    Restore watermelon API request and display
    Reintroduced requests to fetch watermelon data and display it.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :balloon: {st.__version__}")
@@ -54,5 +51,10 @@ if ingredients_list:
 
    # if ingredients_string:
     #    session.sql(my_insert_stmt).collect()
-    
+
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
      #   st.success('Your Smoothie is ordered!', icon="✅")
