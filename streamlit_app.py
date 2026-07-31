1ef22b085b7ad3128c41acb1dc928431a8a7db86
MorgenstiernePrivat <marcus.morgenstierne@icloud.com>
2026-07-31 11:12:54 +0200

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import when_matched
+
+# Write directly to the app
+st.title(f"Customize Your Smoothie! :balloon: {st.__version__}")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+
+  """
+)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted = st.button('Submit')
+    if submitted:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+
+        try:
+            og_dataset.merge(edited_dataset, (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                    )
+            
+            st.success('Order(s) Updated!', icon = '👍')
+        except:
+            st.write('something went wrong.')
+
+else:
+    st.success('There are o pending orders right now!', icon = '👍')
